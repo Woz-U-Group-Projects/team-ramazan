eb2cd7e768908cedc5fcea83024e6dec19d1a4e1
rkoyuncu09 <r.koyuncu@yahoo.com>
2020-06-03 17:08:15 -0700

    fixed navbar

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { SignupComponent } from './signup/signup.component';
 
 
 
+
 const routes: Routes = [
 
 
--- client-angular/src/app/nav/nav.component.ts
+++ client-angular/src/app/nav/nav.component.ts
@@ -11,5 +11,11 @@ export class NavComponent implements OnInit {
 
   ngOnInit() {
   }
+  navbarOpen = false;
 
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+  
+  
 }
